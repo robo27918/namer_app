7d27cc5582b21d8769531a5c2f653ade288f223c
Roberto Toribio <roberttoribio04@gmail.com>
2025-02-21 10:04:16 -0800

    First commit

---
+++ namer_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+         Text(appState.current.asLowerCase),
+          Text("This is some other text.... and it just goes herw"),
+          Text("Finally we have this other line"),
+          Text('Quite cool if i say so myself'),
+          ElevatedButton(onPressed:(){print("button pressed!");}, child: Text("Next"))
+        ],
+      ),
+    );
+  }
+}
